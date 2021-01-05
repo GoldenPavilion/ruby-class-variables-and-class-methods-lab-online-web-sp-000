6f00665be9b17ea9debed5ed06b73711a69eb1dc
Robert Stine <rcorystine@gmail.com>
2021-01-05 17:52:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,6 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each {|genre| genre_count[genre] += 1}
-    binding.pry
     genre_count
   end
   
